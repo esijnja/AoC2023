8dd599404a3ff7ecf5e9e58ad03fbd5cde8adae8
Erik Sijnja <esijnja@xs4all.nl>
2023-12-05 22:10:54 +0100

    AoC 2023 day 01 part 1

---
+++ day01/day01part1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+// read input
+var input = File.ReadAllLines("input.txt");
+// for each line
+// get first number
+// get last number
+// line value = first number * 10 + last number
+int GetFirstNumber(string line)
+{
+    foreach (var c in line)
+    {
+        if (char.IsDigit(c))
+        {
+            return int.Parse(c.ToString());
+        }
+    }
+    return 0;
+}
+
+static string Reverse( string s )
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
+int GetLastNumber(string line)
+{
+    return GetFirstNumber(Reverse(line));
+}
+
+var value = 0;
+foreach (var line in input)
+{
+    var firstNumber = GetFirstNumber(line);
+    var lastNumber = GetLastNumber(line);
+    var lineValue = firstNumber * 10 + lastNumber;
+    value += lineValue;
+}
+
+Console.WriteLine(value);
