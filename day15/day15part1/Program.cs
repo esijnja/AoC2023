bd61c97c30078b2733f931d05b7582fb0ff31f6a
Erik Sijnja <esijnja@xs4all.nl>
2023-12-15 23:20:11 +0100

    AoC day15 part 1

---
+++ day15/day15part1/Program.cs
@@ -0,0 +1,18 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var keys = input[0].Split(',').ToList();
+var total = 0;
+foreach (var key in keys)
+{
+    var current = 0;
+    var chars = key.ToCharArray();
+    foreach (var c in chars)
+    {
+        current += (int)c;
+        current *= 17;
+        current %= 256; 
+    }
+    Console.WriteLine(current);
+    total += current;
+}
+Console.WriteLine(total);
